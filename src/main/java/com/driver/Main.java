8b5f1a14f55b1b97caa7b10904f0ba4098d0e5c1
sagarkaimwal <sagarkemwal096@gmail.com>
2023-03-09 16:16:44 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[])
+    {
+        RWOnly a=new RWOnly();
+
+        //System.out.println("'name' has private access in 'com.driver.RWOnly'");
+        a.setName("sagar");
+        System.out.println(a.getName());
+    }
 }
